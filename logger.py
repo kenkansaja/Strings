c2cbddb3cec85912800563ec8f8c7744e6216649
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-06-03 19:58:04 +0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -6,5 +6,5 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-
+#Bot siap di gunakan
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
