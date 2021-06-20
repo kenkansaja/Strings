b1395506de4163262d67f6838a54cc64342f33c4
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-20 19:05:51 +0700

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -3,7 +3,7 @@ import asyncio
 from bot import bot, HU_APP
 from pyromod import listen
 from asyncio.exceptions import TimeoutError
-
+import string_session
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import (
