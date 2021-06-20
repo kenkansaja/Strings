5cf10f9eb9d622f184a5641c6d9e6a2b594b6328
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-20 19:04:51 +0700

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # (c) https://t.me/TelethonChat/37677 and SpEcHiDe
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
+
 
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-print("""Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details
-Check your Telegram saved messages section to copy the STRING_SESSION""")
+@bot.on_message(filters.private & filters.command ("teleton"))
+async def string_session(_, msg: Message):
+print("""Silakan masuk ke my.telegram.org atau @Kenkanrobot
+Masuk menggunakan akun Telegram Anda
+Klik pada Alat Pengembangan API
+Buat aplikasi baru, dengan memasukkan detail yang diperlukan
+Periksa bagian pesan tersimpan Telegram Anda untuk menyalin STRING_SESSION""")
 API_KEY = int(input("Enter API_KEY here: "))
 API_HASH = input("Enter API_HASH here: ")
 
@@ -23,5 +23,5 @@ with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
 
 <code>STRING_SESSION</code>: <code>{}</code>
 
-⚠️ <i>Please be careful before passing this value to third parties</i>""".format(session_string)
+⚠️ <i>Harap berhati-hati sebelum memberikan nilai ini ke pihak ketiga</i>""".format(session_string)
     client.send_message("me", saved_messages_template, parse_mode="html")
